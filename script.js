f0a69d9266df4f079e52e317fa30cc20d7f2f798
LeonardoTalanski <leonardo.e.t@hotmail.com>
2023-09-12 23:44:13 -0300

    Adicionando cons tools, draw, sizeButtons

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
 
+const inputColor = document.querySelector(".input__color")
+const tools = document.querySelectorAll(".tool")
+const sizeButtons = document.querySelector(".button__size")
+const buttonClear = document.querySelector(".button__clear")
+
+let brushSize = 10
+
+const draw = (x, y) => {
+    ctx.fillStyle = "#000"
+    ctx.fillRect (x, y, brushSize, brushSize)
+}
+
