f6983f003e6a5e4da6a4aa77d5c0b0ce8de51dc8
LeonardoTalanski <leonardo.e.t@hotmail.com>
2023-09-12 23:52:23 -0300

    Corrigindo problema de draw X;y

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ const tools = document.querySelectorAll(".tool")
 const sizeButtons = document.querySelector(".button__size")
 const buttonClear = document.querySelector(".button__clear")
 
-let brushSize = 10
+let brushSize = 50
+
+canvas.addEventListener("mousedown", (event) => { 
+    const { clientX, clientY } = event
+    draw(clientX, clientY)
+})
 
 const draw = (x, y) => {
     ctx.fillStyle = "#000"
